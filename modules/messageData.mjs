74f56841c336cfb12d9f3a689a09a53de78eb8ac
lquinsol <lquinsol@gmail.com>
2022-12-30 15:35:44 -0500

    Working Project

--- modules/messageData.mjs
+++ modules/messageData.mjs
@@ -1,30 +1,31 @@
-const messages = [{quote:"The best way to predict your future is to create it", author:"Abraham Lincoln"},
-                  {quote:"The future belongs to those who believe in the beauty of their dreams.", author:"Eleanor Roosevelt" },
-                  {quote:"You are braver than you believe, stronger than you seem and smarter than you think", author:"A.A. Milne"},
-                  {quote:"The man who does not read books has no advantage over the one who cannot read them", author:"Mark Twain"},
-                  {quote:"Education is the passport to the future, for tomorrow belongs to those who prepare for it today", author:"Malcolm X"},
-                  {quote:"Teachers can open the door, but you must enter it yourself", author:"Chinese proverb"},
-                  {quote:"The beautiful thing about learning is that no one can take it away from you.", author:"B.B. King"},
-                  {quote:"Education is the most powerful weapon you can use to change the world.", author:"B.B. King"},
-                  {quote:"Don’t let what you cannot do interfere with what you can do.", author:"John Wooden"},
-                  {quote:"A person who never made a mistake never tried anything new", author:"Albert Einstein"},
-                  {quote:"Never let the fear of striking out stop you from playing the game", author:"Babe Ruth"},
-                  {quote:"You don’t have to be great to start, but you have to start to be great.", author:"Zig Ziglar"},
-                  {quote:"The expert in anything was once a beginner", author:"Helen Hayes"},
-                  {quote:"The way to get started is to quit talking and begin doing", author:"Walt Disney"},
-                  {quote:"There are no shortcuts to any place worth going", author:"Beverly Stills"},
-                  {quote:"I think it’s possible for ordinary people to choose to be extraordinary.", author:"Elon Musk"},
-                  {quote:"Genius is 10% inspiration, 90% perspiration", author:"Thomas Edison"},
-                  {quote:"Motivation is what gets you started. Habit is what keeps you going.", author:"Jim Ryun"},
-                  {quote:"Success is the sum of small efforts, repeated", author:"R. Collier"},
-                  {quote:"None of us is as smart as all of us", author:"Ken Blanchard"}                
+const messages = [{quote:"The best way to predict your future is to create it", author:"Abraham Lincoln", subject:"future"},
+                  {quote:"The future belongs to those who believe in the beauty of their dreams.", author:"Eleanor Roosevelt", subject:"future" },
+                  {quote:"You are braver than you believe, stronger than you seem and smarter than you think", author:"A.A. Milne", subject:"future"},
+                  {quote:"The man who does not read books has no advantage over the one who cannot read them", author:"Mark Twain", subject:"education"},
+                  {quote:"Education is the passport to the future, for tomorrow belongs to those who prepare for it today", author:"Malcolm X", subject:"education"},
+                  {quote:"Teachers can open the door, but you must enter it yourself", author:"Chinese proverb", subject:"education"},
+                  {quote:"The beautiful thing about learning is that no one can take it away from you.", author:"B.B. King", subject:"education"},
+                  {quote:"Education is the most powerful weapon you can use to change the world.", author:"B.B. King", subject:"education"},
+                  {quote:"Don’t let what you cannot do interfere with what you can do.", author:"John Wooden", subject:"education"},
+                  {quote:"A person who never made a mistake never tried anything new", author:"Albert Einstein", subject:"education"},
+                  {quote:"Never let the fear of striking out stop you from playing the game", author:"Babe Ruth", subject:"education"},
+                  {quote:"You don’t have to be great to start, but you have to start to be great.", author:"Zig Ziglar", subject:"procrastination"},
+                  {quote:"The expert in anything was once a beginner", author:"Helen Hayes", subject:"procrastination"},
+                  {quote:"The way to get started is to quit talking and begin doing", author:"Walt Disney", subject:"procrastination"},
+                  {quote:"There are no shortcuts to any place worth going", author:"Beverly Stills", subject:"hard work"},
+                  {quote:"I think it’s possible for ordinary people to choose to be extraordinary.", author:"Elon Musk", subject:"hard work"},
+                  {quote:"Genius is 10% inspiration, 90% perspiration", author:"Thomas Edison", subject:"hard work"},
+                  {quote:"Motivation is what gets you started. Habit is what keeps you going.", author:"Jim Ryun", subject:"hard work"},
+                  {quote:"Success is the sum of small efforts, repeated", author:"R. Collier", subject:"hard work"},
+                  {quote:"None of us is as smart as all of us", author:"Ken Blanchard", subject:"students"},
+                  {quote:"In a world where you can be anything, be kind", author:"Jennifer Dukes Lee", subject:"students"},
+                  {quote:"He who asks a question is a fool for five minutes; he who does not ask a question remains a fool forever", author:"Chinese proverb", subject:"students"}             
                 ]
 
-const getQuote = function(index){
-    return messages[index].quote;
-}
+const mindSets = ["active","lazy", "concern", "lost"];
 
 
-export{messages, getQuote};
+
+export{messages, mindSets, getQuote};
 
 
--- modules/messageGenerator.mjs
+++ modules/messageGenerator.mjs
@@ -1,8 +1,27 @@
-import {messages, getQuote} from "./messageData.mjs";
+import {messages, mindSets} from "./messageData.mjs";
 
+//Helper function
+const randomIndex = list => Math.floor(Math.random()* list.length);
 
-const randomQoute = function() {
-    return messages[Math.floor(Math.random()* messages.length)].quote;
-};
+//Module Core
+const advice = function() {
+    //initializing argument in case this function does not received from.
+    let mindSet = mindSets[randomIndex(mindSets)];
+    
+    // Locating resource base in data entry. Finding best qoute base on mood selected.
+    let messagesbyMindSet = [];
+    
+    switch (mindSet){
+        case 'active': messagesbyMindSet = messages.filter(message => {if(message.subject === "hard work") return message}); break;
+        case 'lazy': messagesbyMindSet = messages.filter(message => {if(message.subject === "procrastination") return message});break;
+        case 'concern': messagesbyMindSet = messages.filter(message => {if(message.subject === "future") return message});break;
+        case 'lost': messagesbyMindSet = messages.filter(message => {if(message.subject === "students") return message});break;
+        default : messagesbyMindSet = messages.filter(message => {if(message.subject === "education") return message});
+    }
+    messagesbyMindSet = messagesbyMindSet[randomIndex(messagesbyMindSet)];
+    return `When you feel ${mindSet}, always remember \"${messagesbyMindSet.quote}\", if you want to empower yourself follow ${messagesbyMindSet.author}..`;
+    
 
-console.log(randomQoute());
+}
+
+console.log(advice());
